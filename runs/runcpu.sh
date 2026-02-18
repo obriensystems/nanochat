77cfb0612b801e6002242213b090169fa5314734
Michael OBrien <michael@obrienlabs.org>
2026-02-18 15:29:37 -0500

    #7 - adjust script for perf

--- runs/runcpu.sh
+++ runs/runcpu.sh
@@ -16,18 +16,28 @@
 #export OMP_NUM_THREADS=$(sysctl -n hw.ncpu)
 #export MKL_NUM_THREADS=$(sysctl -n hw.ncpu)
 
-export DEPTH=8
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+echo $timestamp
+START_TIME=$(date +%s)
+
+rm -rf ~/.cache/nanochat/chatsft_checkpoints/* -f
+rm -rf ~/.cache/nanochat/base_checkpoints/* -f
+rm -rf ~/.cache/nanochat/base_eval/* -f
+
+export DEPTH=5
 # note Tokens / micro-batch: must be = or < total batch of 16k
-export BATCH_SIZE=16
+export BATCH_SIZE=32
 export MODEL_TAG="d${DEPTH}"
 
 echo "Running: depth:$DEPTH batch:$BATCH_SIZE model:$MODEL_TAG"
+
+python -m nanochat.report reset
 
 export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
 mkdir -p $NANOCHAT_BASE_DIR
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 [ -d ".venv" ] || uv venv
-uv sync --extra gpu
+uv sync --extra cpu
 source .venv/bin/activate
 if [ -z "$WANDB_RUN" ]; then
     WANDB_RUN=dummy
@@ -35,44 +45,55 @@ fi
 
 # train tokenizer on ~2B characters (~34 seconds on my MacBook Pro M3 Max)
 python -m nanochat.dataset -n 8
+# Immediately also kick off downloading more shards in the background while tokenizer trains
+# Approximately 350 shards are needed for 10B tokens of data for pretraining.
+# The maximum total number of shards available in the entire dataset is 1822.
+python -m nanochat.dataset -n 370 &
+DATASET_DOWNLOAD_PID=$!
 python -m scripts.tok_train --max-chars=2000000000
 python -m scripts.tok_eval
 
-# train a small 4 layer model
-# I tuned this run to complete in about 30 minutes on my MacBook Pro M3 Max.
-# To get better results, try increasing num_iterations, or get other ideas from your favorite LLM.
+echo "Waiting for dataset download to complete..."
+wait $DATASET_DOWNLOAD_PID
+
 echo "base_train"
+# 4 batch for d26
 python -m scripts.base_train \
     --depth=$DEPTH \
     --head-dim=64 \
     --window-pattern=L \
     --max-seq-len=512 \
     --device-batch-size=$BATCH_SIZE \
-    --total-batch-size=16384 \
     --eval-every=100 \
     --eval-tokens=524288 \
     --core-metric-every=-1 \
     --sample-every=100 \
-    --num-iterations=5000 \
+    --num-iterations=2000 \
     --run=$WANDB_RUN
+    #    --fp8 \
+    #--target-param-data-ratio=8.25 \
+
 echo "base_eval"
 python -m scripts.base_eval --device-batch-size=1 --split-tokens=16384 --max-per-task=16
+#python -m scripts.chat_eval -i sft -g $MODEL_TAG
 
-
-# SFT (~10 minutes on my MacBook Pro M3 Max)
+# SFT
 curl -L -o $NANOCHAT_BASE_DIR/identity_conversations.jsonl https://karpathy-public.s3.us-west-2.amazonaws.com/identity_conversations.jsonl
 echo "chat_sft"
-# output to ~/.cache/nanochat/chatsft_checkpoints/d$DEPTH = $MODEL_TAG
 python -m scripts.chat_sft \
-    --max-seq-len=512 \
     --device-batch-size=$BATCH_SIZE \
-    --total-batch-size=16384 \
     --eval-every=200 \
-    --eval-tokens=524288 \
-    --num-iterations=1500 \
     --model-tag=$MODEL_TAG \
+    --num-iterations=1500 \
     --run=$WANDB_RUN
+    #--max-seq-len=512 \
+    #--eval-tokens=524288 \
+    #--num-iterations=1500 \
+    #--total-batch-size=16384 \
 
+# python -m scripts.chat_eval -i sft -g $MODEL_TAG
+
+python -m nanochat.report generate
 # Chat with the model over CLI
 # The model should be able to say that it is Paris.
 # It might even know that the color of the sky is blue.
@@ -82,4 +103,8 @@ python -m scripts.chat_cli -p "What is the capital of France?" --model-tag $MODE
 # Chat with the model over a pretty WebUI ChatGPT style
 # python -m scripts.chat_web
 
-echo "Ran: depth:$DEPTH batch:$BATCH_SIZE model:$MODEL_TAG"
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+echo $timestamp
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+echo "duration, $ELAPSED_TIME, depth, $DEPTH, batch, $BATCH_SIZE, model, $MODEL_TAG"
--- scripts/chat_sft.py
+++ scripts/chat_sft.py
@@ -208,6 +208,7 @@ def sft_data_generator_bos_bestfit(split, buffer_size=100):
         nonlocal cursor, epoch
         while len(conv_buffer) < buffer_size:
             conversation = dataset[cursor]
+            # Avoid BOS - beginning of sequence - only output
             ids, _ = tokenizer.render_conversation(conversation, max_tokens=row_capacity)
             conv_buffer.append(ids)
             cursor += ddp_world_size
